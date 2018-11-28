a603596fe7d4d7ac6f9244c82c2551bfbeb0595a
fadicool <fadeleadu@gmail.com>
2018-11-28 12:38:48 +0000

    Update Demo.java

--- RESTAssuredDemo/src/test/java/RESTAssuredDemo/Demo.java
+++ RESTAssuredDemo/src/test/java/RESTAssuredDemo/Demo.java
@@ -21,6 +21,7 @@ public class Demo {
                 then().
                     assertThat().
                     body("MRData.CircuitTable.Circuits.circuitId",hasSize(20));
+        System.out.pringln("Number of Circuits validated");
     }
 
     @Test
